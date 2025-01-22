fd61efdb5e21d0e5bcc654b87df2af385dab3e70
root <root@ip-172-31-93-58.ec2.internal>
2025-01-22 07:01:19 +0000

    This is my second commit

---
+++ math.sh
@@ -0,0 +1,7 @@
+echo "Enter first number:"
+read firstNumber
+
+echo "Enter second number:"
+read secondNumber
+
+echo "The result of $firstNumber * $secondNumber is: `expr $firstNumber \*  $secondNumber`"
